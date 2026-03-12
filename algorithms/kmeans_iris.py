5c5b30a8994c0d7af452db9f1ade1985a5fc749f
Heanaabel <hanaabeldjilali777@gmail.com>
2026-03-12 18:39:39 +0000

    KNN method added

--- algorithms/kmeans_iris.py
+++ algorithms/kmeans_iris.py
@@ -1,7 +1,9 @@
+
 from sklearn import datasets
 from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 def run_kmeans():
     iris = datasets.load_iris()
@@ -11,8 +13,19 @@ def run_kmeans():
     kmeans.fit(X)
     labels = kmeans.labels_
 
-    plt.scatter(X.iloc[:,2], X.iloc[:,3], c=labels, cmap='viridis')
+    return X.values, kmeans, labels
+
+def plot_kmeans(X, kmeans_model, labels=None):
+    if labels is None:
+        labels = kmeans_model.labels_
+
+    plt.figure(figsize=(8, 6))
+    plt.scatter(X[:, 2], X[:, 3], c=labels, cmap='viridis', s=60, edgecolors='k', alpha=0.7)
+    plt.scatter(kmeans_model.cluster_centers_[:, 2], kmeans_model.cluster_centers_[:, 3],
+                c='red', marker='X', s=200, label='Centres')
     plt.xlabel('Petal Length')
     plt.ylabel('Petal Width')
     plt.title("KMeans Clustering Iris")
+    plt.legend()
+    plt.grid(alpha=0.3)
     plt.show()
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from algorithms.lda_wine import run_lda
-from algorithms.kmeans_iris import run_kmeans
+from algorithms.kmeans_iris import run_kmeans, plot_kmeans
 from algorithms.decision_tree_iris import run_decision_tree
 from algorithms.regression_housing import run_regression
-from algorithms.svm import run_svm   # <-- ici le changement
+from algorithms.Svm import run_svm
+from algorithms.knn_iris import run_knn, plot_knn
 
 def main():
     print("\n===== LDA WINE CLASSIFICATION =====")
     run_lda()
 
-    print("\n===== KMEANS IRIS CLUSTERING =====")
-    run_kmeans()
+    X, kmeans_model, labels = run_kmeans()
+    plot_kmeans(X, kmeans_model, labels)
 
     print("\n===== DECISION TREE IRIS =====")
     run_decision_tree()
